e0f35d8fa6a8cf04fd0b037161ccf2404895596b
xiepengfei <xiepengfei@wezhuiyi.com>
2018-07-27 20:03:39 +0800

    fix

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -2,7 +2,7 @@ import { Controller } from 'egg';
 import {GET, PREFIX, Query, Validate} from '../lib/egg-request-mapping';
 import {IsInt} from 'class-validator';
 
-export class createDto {
+export class CreateDto {
     @IsInt()
     id: number;
 }
@@ -11,7 +11,7 @@ export class createDto {
 export default class HomeController extends Controller {
     @GET('index')
     @Validate()
-    public async index(@Query() body: object, @Query() query: createDto) {
+    public async index(@Query() body: object, @Query() query: CreateDto) {
         console.log(body);
         console.log(query);
         const { ctx } = this;
